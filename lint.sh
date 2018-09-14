47a2b0dc84d2fea6f17a92b0bd328e87f5859ce4
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2018-09-14 10:37:25 +0200

    commit

--- lint.sh
+++ lint.sh
@@ -3,3 +3,4 @@
 helm lint helloworld-simple
 helm lint helloworld-db
 helm lint helloworld-ingress
+helm lint api-gateway
--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-version=0.1.1
+version=0.1.2
 
 helm package --version $version helloworld-simple
 helm package --version $version helloworld-db
 helm package --version $version helloworld-ingress
+helm package --version $version api-gateway
 
 helm repo index . --url https://helloworld-k8s.github.io/charts/
 
